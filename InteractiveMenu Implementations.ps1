d9b95ec69da3c6f147da92e180f23d4c82283d82
Reggie Wilson <wilsonrg5338@gmail.com>
2022-06-08 17:35:32 -0500

    Add files via upload

---
+++ InteractiveMenu Implementations.ps1
@@ -0,0 +1,16 @@
+﻿do
+{
+    Show-Menu -Title "TechSnips"
+    $UserInput = Read-Host "Please make a selection"
+    switch ($UserInput)
+    {
+        '1'{'You chose option #1'}
+        '2'{'You chose option #2'}
+        '3'{'You chose option #3'}
+
+    }
+    pause
+
+}
+
+until ($UserInput -eq 'q')
